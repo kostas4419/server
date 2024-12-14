7d26330b3471432a345ac96f9b9fc0f183b059e0
kostas4419 <47432222+kostas4419@users.noreply.github.com>
2024-12-14 13:47:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,15 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: "*"
+    origin: process.env.CLIENT_URL
 }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use('/api', router)
 app.use(errorMiddleware)
 
