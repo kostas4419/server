58a41b6f902217ef9e83c05d7858a18c82fec86c
kostas4419 <47432222+kostas4419@users.noreply.github.com>
2024-12-14 13:48:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
     origin: process.env.CLIENT_URL
 }))
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+    res.header("Access-Control-Allow-Origin", 'http://test-crmleads.ru:3000');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
